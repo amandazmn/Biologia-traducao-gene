bdff9b6ab2c2b5812362f8a2ca55c4aa4de3fc03
Amanda Zimmermann <124809205+amandazmn@users.noreply.github.com>
2023-05-03 16:33:35 -0300

    Add files via upload

---
+++ sistemaTraducaoCodGenetico/src/sistemaTraducaoCodGenetico/Acido.java
@@ -0,0 +1,30 @@
+package sistemaTraducaoCodGenetico;
+
+public class Acido {
+	private String tipo;
+	private String nome;
+	private String codigo;
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo() {
+		this.tipo = tipo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome() {
+		this.nome = nome;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo() {
+		this.codigo = codigo; 
+	}
+	public void cadastrarAcido() {
+		
+	}
+	
+}
